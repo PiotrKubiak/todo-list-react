c64ce6912c8ff25a576c62d719aab22c90c1d507
Piotr Kubiak <kuberxxx1986@gmail.com>
2021-06-17 10:33:43 +0200

    remove tasks interaction

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import Header from "./Header";
 import Container from "./Container";
 import { useState } from "react";
 
-const tasks = [
-  { id: 1, content: "przejść na Reacta", done: false },
-  { id: 2, content: "wypić YerbaMate", done: true },
-];
-
 function App() {
   const [hideDone, setHideDone] = useState(false);
+  const [tasks, setTasks] = useState([
+    { id: 1, content: "przejść na Reacta", done: false },
+    { id: 2, content: "wypić YerbaMate", done: true },
+  ]);
 
   const toggleHideDone = () => {
     setHideDone((hideDone) => !hideDone);
+  };
+
+  const removeTask = (id) => {
+    setTasks((tasks) => tasks.filter((task) => task.id !== id));
   };
 
   return (
@@ -24,7 +27,9 @@ function App() {
       <Section title="Dodaj nowe zadanie" body={<Form />} />
       <Section
         title="Lista zadań"
-        body={<Tasks tasks={tasks} hideDones={hideDone} />}
+        body={
+          <Tasks tasks={tasks} hideDones={hideDone} removeTask={removeTask} />
+        }
         extraHeaderContent={
           <Buttons
             tasks={tasks}
