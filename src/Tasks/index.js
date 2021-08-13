5fbcd2eafbb4800aba2b41d100e86cc67c8a2eb4
Piotr Kubiak <kuberxxx1986@gmail.com>
2021-08-13 09:22:13 +0200

    added S.C. for Task index.js

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,36 +1,19 @@
-import "./style.css";
+import { List, Item, Content, Button } from "./styled";
 
 const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
-  <ul className="tasks">
+  <List>
     {tasks.map((task) => (
-      <li
-        key={task.id}
-        className={`tasks__item ${
-          task.done && hideDone ? "tasks__item tasks__item--hidden" : ""
-        }`}
-      >
-        <button
-          className="tasks__button tasks__button--toggleDone"
-          onClick={() => toggleTaskDone(task.id)}
-        >
+      <Item key={task.id} hidden={task.done && hideDone}>
+        <Button toggleDone onClick={() => toggleTaskDone(task.id)}>
           {task.done ? "✔" : ""}
-        </button>
-        <span
-          className={`tasks__content${
-            task.done ? " tasks__content--done" : ""
-          }`}
-        >
-          {task.id} - {task.content}
-        </span>
-        <button
-          className="tasks__button tasks__button--delete"
-          onClick={() => removeTask(task.id)}
-        >
+        </Button>
+        <Content done={task.done}>{task.content}</Content>
+        <Button remove onClick={() => removeTask(task.id)}>
           🗑
-        </button>
-      </li>
+        </Button>
+      </Item>
     ))}
-  </ul>
+  </List>
 );
 
 export default Tasks;
